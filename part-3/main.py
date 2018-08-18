c87c100273d31bb22f72a018dcb5e5be6f107306
Matas <32897367+matasgit@users.noreply.github.com>
2018-08-18 08:58:28 +0100

    Update main.py

--- part-3/main.py
+++ part-3/main.py
@@ -40,11 +40,12 @@ def add_padding(padding, image_width, image_height, x, y, w, h):
     return x, y, w, h
 
 
-LIMIT = 50
-MAX_OFFSET = 99951  # maximum offset allowed by IMDb (99,951+50-1 = 100,000 results)
+LIMIT = 100
+MAX_OFFSET = 4999901  # maximum offset (4,999,901+100-1 = 5,000,000 results)
 FILE_SAVE_PATH = 'images/'
 FACE_FILE_SAVE_PATH = 'images/faces/'
 UNRESOLVED_FILE_SAVE_PATH = 'images/unresolved/'
+LOG_PATH = 'log.txt'
 FACE_SIZE = 120  # pixels
 FACE_PADDING = 20  # percent
 DISPLAY_PICTURES = True  # set this to True for easier debugging
@@ -52,6 +53,7 @@ DISPLAY_PICTURES = True  # set this to True for easier debugging
 offset = 1
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 total_success = 0
+list_url = 'https://www.imdb.com/search/name?gender=male,female&count=100'
 
 if not os.path.exists(FILE_SAVE_PATH):
     os.makedirs(FILE_SAVE_PATH)
@@ -68,12 +70,14 @@ while offset <= MAX_OFFSET:
         print('Loading list...')
         print('Offset:', offset, '\n')
 
-        list_url = 'http://www.imdb.com/search/name?gender=male,female&start=' + str(offset)
         list = requests.get(list_url)
+        print('List URL:', list_url, '\n')
 
         if list.status_code == 200:
 
             soup = BeautifulSoup(list.content, 'html.parser')
+            next_list_url = soup.select('.lister-page-next.next-page')
+            next_list_url = 'https://www.imdb.com' + next_list_url[0]['href']
             links = soup.select('h3.lister-item-header > a')
 
             for link in links:
@@ -176,6 +180,11 @@ while offset <= MAX_OFFSET:
                         shutil.copyfile(actor_picture_save_path, UNRESOLVED_FILE_SAVE_PATH + file_name)
                         os.remove(actor_picture_save_path)
                     pass
+
+            log_file = open(LOG_PATH, 'w')
+            log_file.write("Offset: " + str(offset) + '\n' + list_url + '\n\n')
+            log_file.close()
+            list_url = next_list_url
 
         else:
             print(list_url, 'HTTP status:', list.status_code, '\n')
